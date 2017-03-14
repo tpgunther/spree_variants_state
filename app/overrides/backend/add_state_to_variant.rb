897df2fa6194a3cc23a06e05d5c6c53744dfa78f
Tomás Günther <tpgunther@acid.cl>
2017-03-14 17:51:46 -0300

    add state to admin

---
+++ app/overrides/backend/add_state_to_variant.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(:virtual_path => 'spree/admin/variants/_form',
+  :name => 'add_state_to_variant',
+  :insert_bottom => "div[data-hook='admin_variant_form_additional_fields']",
+  :text => "
+    <div class='field' data-hook='admin_variants_form_state'>
+      <%= f.label :state, raw(Spree.t(:state)) %>
+      <%= select(@variant, :state, options_for_select([['Activo','state_active'], ['Descontinuo', 'state_descontinued'], ['Discontinuo', 'state_discontinued']], @variant.state), {}, class: 'select2 fullwidth select2-offscreen') %>
+    </div>
+  ")
