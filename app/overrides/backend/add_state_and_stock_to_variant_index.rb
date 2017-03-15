dd8836a9ae0ab49a19c00d904521c67e26753ae1
Tomás Günther <tpgunther@acid.cl>
2017-03-15 11:53:30 -0300

    add stock and state to variant index

---
+++ app/overrides/backend/add_state_and_stock_to_variant_index.rb
@@ -0,0 +1,15 @@
+Deface::Override.new(:virtual_path => 'spree/admin/variants/index',
+  :name => 'add_state_and_stock_to_variant_index_1',
+  :insert_before => "thead[data-hook='variants_header'] th.actions",
+  :text => "
+      <th>Stock</th>
+      <th>Estado</th>
+  ")
+
+Deface::Override.new(:virtual_path => 'spree/admin/variants/index',
+  :name => 'add_state_and_stock_to_variant_index_2',
+  :insert_before => "tbody td.actions",
+  :text => "
+      <td class='align-center'><%= variant.total_on_hand %></td>
+      <td class='align-center'><%= variant.state_name %></td>
+  ")
